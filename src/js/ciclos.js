dead2164c10b40450124c37fdc5759736f4e10a8
samuel <samueroncancio@gmail.com>
2026-05-07 13:50:04 -0500

    hoy practique

---
+++ src/js/ciclos.js
@@ -0,0 +1,35 @@
+//imprime  los numeros del 1 al 10 usando ciclo for:
+for (let i = 0; i<=10; i++){
+    console.log("numero "+ i);
+
+}
+console.log("-----------------------------")
+
+// usar forEach
+const animales = ["🐶", "🐱", "🐭", "🐹"];
+animales.forEach(function(animal){
+    console.log(animal + "❤️");
+})
+console.log("-----------------------------")
+//usar while 
+let numero = 5;
+while(numero >= 1){
+    console.log(numero);
+    numero--;
+}
+console.log("-----------------------------")
+//suma de array con for
+const nums = [5, 10, 15, 20];
+let suma = 0;
+for (let i = 0; i < nums.length; i++) {
+  suma += nums[i];
+}
+console.log("Suma:", suma);
+
+console.log("-----------------------------")
+//usa forEach
+const numerosDos = [2, 4, 6, 8];
+let cuadrado = []
+numerosDos.forEach(function(cuadrado){
+    console.log("El cuadrado de cada numero es " **)
+})
